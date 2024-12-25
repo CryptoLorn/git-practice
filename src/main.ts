9942c4695ba4296272511c070d32248d5748a7d2
CryptoLorn <91628137+CryptoLorn@users.noreply.github.com>
2024-12-25 15:26:32 +0200

    add test type

--- src/main.ts
+++ src/main.ts
@@ -24,5 +24,6 @@ app.use(
 
 app.listen(config.port, async () => {
   await mongoose.connect(config.mongoUri);
+  console.log("some changed");
   console.log(`Server has been started on port ${config.port}`);
 });
